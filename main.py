50e13995d013463eb8936ebb03fd8186cc6cd0da
huahua2017 <1035520697@qq.com>
2023-03-02 12:33:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -234,5 +234,5 @@ if __name__ == "__main__":
     note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, city, weather, max_temperature, min_temperature,humidity,wind, airData,airQuality,note_ch, note_en)
+        send_message(user, accessToken, city, weather, max_temperature, min_temperature,wind, airData,airQuality,note_ch, note_en)
     os.system("pause")
